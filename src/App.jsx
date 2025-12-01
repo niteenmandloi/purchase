2013a8a9e4766fe1a077b914cad6f22e42eb0901
satyam-malgaya <satyam.malgaya@gmail.com>
2025-12-01 22:48:16 +0530

    solving error

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,8 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer"
 import Order from "./pages/Order";
-<<<<<<< HEAD
 import Home from './pages/Home.jsx';
-
-=======
-import Home from './pages/Home';
 import Header from './components/Header';
->>>>>>> eb2f9c940cd1ad4937cc45d5b715ea3abeb57807
 const App = () => {
   return (
     <div>
