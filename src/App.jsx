a408227c23cb4a5b167a7b7520f3228da04aab4d
satyam-malgaya <satyam.malgaya@gmail.com>
2025-12-01 22:50:17 +0530

    addding

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ const App = () => {
       <Route path="/" element={<Home />} />
         <Route path="/order" element={<Order />} />   {/* <-- Path for navigate */}
       </Routes>
-      <Header/>
       <Footer/>
 
     </div>
